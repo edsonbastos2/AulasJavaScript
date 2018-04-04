0f7b88747cba4352213d57c5ceb84a99e4be4ba4
edsonbastos <edsonbastos2@hotmail.com>
2018-04-04 13:49:55 -0300

    Boolean exemplo

---
+++ fundamentos/boolean.js
@@ -0,0 +1,13 @@
+let isAtivo = false
+console.log(isAtivo)
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+console.log(!!('' || null || 0 || ' '))
+
+let nome = ''
+
+console.log(nome || 'Desconhecido')
+console.log(!!null)
